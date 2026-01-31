2757a57dcf5920fb8820f0dc3ca34b47da527641
vexlee <45237627+vexlee@users.noreply.github.com>
2026-01-31 13:52:07 +0000

    Deploying to gh-pages from @ vexlee/SnapCalAI@ad638e73130a4898cd7c5131f971c61fedc2cb5e 🚀

--- assets/loader-2-CyjC-jWP.js
+++ assets/loader-2-CyjC-jWP.js
@@ -1,6 +1,6 @@
-import{c as o}from"./index-DC3id3Mb.js";/**
+import{c as a}from"./index-C-GE5puo.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const n=o("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{n as C};
+ */const o=a("Loader2",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{o as L};
--- assets/target-JXrSWAbo.js
+++ assets/target-JXrSWAbo.js
@@ -1,4 +1,4 @@
-import{c}from"./index-DC3id3Mb.js";/**
+import{c}from"./index-C-GE5puo.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
