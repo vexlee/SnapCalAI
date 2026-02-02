7006c33efa24810aa95bd6286530ccbea9c36388
vexlee <45237627+vexlee@users.noreply.github.com>
2026-02-02 18:57:21 +0000

    Deploying to gh-pages from @ vexlee/SnapCalAI@6c71c1dc0ed8708fb503e7786aa5c191f5221aa0 🚀

--- assets/chevron-left-BjI3rhyl.js
+++ assets/chevron-left-BjI3rhyl.js
@@ -1,6 +1,6 @@
-import{c}from"./index-hJxi6Bgy.js";/**
+import{c as e}from"./index-aBxdPfl4.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const o=c("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{o as C};
+ */const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
